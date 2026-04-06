ed5574674f760644ffc00285c5e3502133abab05
realnitsuj <jbossard02@gmail.com>
2026-04-05 23:00:01 -0400

    working docker compose

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,8 +19,9 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
+    # "http://localhost",
+    # "http://localhost:8080",
+    "*",
 ]
 
 app.add_middleware(
